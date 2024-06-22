b55b59a445dee009e68a0b3cead35d96a18a1c77
lonegunman82 <mateusz.pietrzyk@gmail.com>
2024-06-22 11:41:05 +0200

    Poprawka LoginForm.js

--- src/main/frontend/src/LoginForm.js
+++ src/main/frontend/src/LoginForm.js
@@ -1,50 +1,16 @@
-import React, { useState } from "react";
 
-export default function LoginForm({ onLogin, buttonLabel }) {
+
+
+
+
+import {useState} from "react";
+
+export default function LoginForm({onLogin, buttonLabel}) {
     const [email, setEmail] = useState('');
 
-    const handleLoginClick = async () => {
-        const response = await fetch(`/api/participants/`, {
-            method: 'POST',
-            body: JSON.stringify({"login": email}),
-            headers: {'Content-Type': 'application/json'}
-        });
-        if (response.ok) {
-
-            onLogin(email);
-        } else {
-            console.error("Błąd podczas logowania");
-        }
-    };
-
-    return (
-        <div>
-            <label>Zaloguj się e-mailem</label>
-            <input
-                type="text"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-            />
-            <button
-                type="button"
-                onClick={handleLoginClick}
-            >
-                {buttonLabel || 'Wchodzę'}
-            </button>
-        </div>
-    );
+    return <div>
+        <label>Zaloguj się e-mailem</label>
+        <input type="text" value={email} onChange={(e) => setEmail(e.target.value)}/>
+        <button type="button" onClick={() => onLogin(email)}>{buttonLabel || 'Wchodzę'}</button>
+    </div>;
 }
-
-
-
-// import {useState} from "react";
-//
-// export default function LoginForm({onLogin, buttonLabel}) {
-//     const [email, setEmail] = useState('');
-//
-//     return <div>
-//         <label>Zaloguj się e-mailem</label>
-//         <input type="text" value={email} onChange={(e) => setEmail(e.target.value)}/>
-//         <button type="button" onClick={() => onLogin(email)}>{buttonLabel || 'Wchodzę'}</button>
-//     </div>;
-// }
