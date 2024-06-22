813e8943de85a76ccc00390bb27317de40ea0e34
lonegunman82 <mateusz.pietrzyk@gmail.com>
2024-06-22 11:25:52 +0200

    Działa dodawanie i usuwanie użytkowników do spotkań.

--- src/main/frontend/src/LoginForm.js
+++ src/main/frontend/src/LoginForm.js
@@ -1,11 +1,50 @@
-import {useState} from "react";
+import React, { useState } from "react";
 
-export default function LoginForm({onLogin, buttonLabel}) {
+export default function LoginForm({ onLogin, buttonLabel }) {
     const [email, setEmail] = useState('');
 
-    return <div>
-        <label>Zaloguj się e-mailem</label>
-        <input type="text" value={email} onChange={(e) => setEmail(e.target.value)}/>
-        <button type="button" onClick={() => onLogin(email)}>{buttonLabel || 'Wchodzę'}</button>
-    </div>;
+    const handleLoginClick = async () => {
+        const response = await fetch(`/api/participants/`, {
+            method: 'POST',
+            body: JSON.stringify({"login": email}),
+            headers: {'Content-Type': 'application/json'}
+        });
+        if (response.ok) {
+
+            onLogin(email);
+        } else {
+            console.error("Błąd podczas logowania");
+        }
+    };
+
+    return (
+        <div>
+            <label>Zaloguj się e-mailem</label>
+            <input
+                type="text"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+            <button
+                type="button"
+                onClick={handleLoginClick}
+            >
+                {buttonLabel || 'Wchodzę'}
+            </button>
+        </div>
+    );
 }
+
+
+
+// import {useState} from "react";
+//
+// export default function LoginForm({onLogin, buttonLabel}) {
+//     const [email, setEmail] = useState('');
+//
+//     return <div>
+//         <label>Zaloguj się e-mailem</label>
+//         <input type="text" value={email} onChange={(e) => setEmail(e.target.value)}/>
+//         <button type="button" onClick={() => onLogin(email)}>{buttonLabel || 'Wchodzę'}</button>
+//     </div>;
+// }
--- src/main/frontend/src/meetings/MeetingsPage.js
+++ src/main/frontend/src/meetings/MeetingsPage.js
@@ -118,6 +118,22 @@
 //         </div>
 //     )
 // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import {useEffect, useState} from "react";
 import NewMeetingForm from "./NewMeetingForm";
 import MeetingsList from "./MeetingsList";
@@ -126,6 +142,7 @@ export default function MeetingsPage({username}) {
     const [meetings, setMeetings] = useState([]);
     const [addingNewMeeting, setAddingNewMeeting] = useState(false);
     const [refreshMeetings, setResfeshMeetings] = useState(false);
+    const [refreshUser,setRefreshUser] = useState(false);
 
     useEffect(() => {
         const fetchMeetings = async () => {
@@ -150,7 +167,7 @@ export default function MeetingsPage({username}) {
             setAddingNewMeeting(false);
         } else {
             alert("Status: " + response.status);
-            setResfeshMeetings(!refreshMeetings);
+            setRefreshUser(!refreshMeetings);
         }
     }
 
@@ -163,37 +180,29 @@ export default function MeetingsPage({username}) {
             setMeetings(nextMeetings);
         } else {
             alert("Status: " + response.status);
-            setResfeshMeetings(!refreshMeetings);
+            setRefreshUser(!refreshUser);
         }
 
     }
-
     async function handleSignIn(meeting) {
-        try {
-            const response = await fetch(`/api/meetings/${meeting.id}/participants`, {
-                method: 'POST',
-                body: JSON.stringify({ login: username }),
-                headers: { 'content-type': 'application/json' }
-            });
+        // const dataToSend = [{ login: username }];
+        const response = await fetch(`/api/meetings/${meeting.id}/participants/`, {
+            method: 'POST',
+            body: JSON.stringify({login: username}),
+            headers: {'Content-Type': 'application/json'}
+        });
+        if (response.ok) {
 
-            if (response.ok) {
-                const newParticipants = await response.json();
-
-                const nextMeetings = meetings.map(m => {
-                    if (m === meeting) {
-                        m.participants = newParticipants;
-                    }
-                    return m;
-                });
-
-                setMeetings(nextMeetings);
-            } else {
-                console.error('Nie udało się dodać uczestnika do spotkania:', response.statusText);
-                // Dodaj odpowiednią obsługę błędów, jeśli to konieczne
+        const nextMeetings = meetings.map(m => {
+            if (m === meeting) {
+                m.participants = [...m.participants, username];
             }
-        } catch (error) {
-            console.error('Wystąpił błąd podczas dodawania uczestnika do spotkania:', error);
-            // Dodaj odpowiednią obsługę błędów, jeśli to konieczne
+            return m;
+        });
+        setMeetings(nextMeetings);
+    }else {
+            alert("Status: " + response.status);
+            setResfeshMeetings(!refreshUser);
         }
     }
 
@@ -225,3 +234,104 @@ export default function MeetingsPage({username}) {
         </div>
     )
 }
+
+
+
+
+/*
+import {useEffect, useState} from "react";
+import NewMeetingForm from "./NewMeetingForm";
+import MeetingsList from "./MeetingsList";
+
+export default function MeetingsPage({username}) {
+    const [meetings, setMeetings] = useState([]);
+    const [addingNewMeeting, setAddingNewMeeting] = useState(false);
+
+    async function handleNewMeeting(meeting) {
+        const response = await fetch('/api/meetings', {
+            method: 'POST',
+            body: JSON.stringify(meeting),
+            headers: {
+                'content-type': 'application/json'
+            }
+        });
+        if (response.ok) {
+            const newMeeting = await response.json();
+            const nextMeetings = [...meetings, newMeeting];
+            setMeetings(nextMeetings);
+            setAddingNewMeeting(false);
+        }
+    }
+
+    async function handleDeleteMeeting(meeting) {
+        const response = await fetch(`/api/meetings/${meeting.id}`, {
+            method: 'DELETE',
+        });
+        if (response.ok) {
+            const nextMeetings = meetings.filter(m => m !== meeting);
+            setMeetings(nextMeetings);
+        }
+    }
+
+    async function handleSignIn(meeting) {
+        const response = await fetch(`/api/meetings/${meeting.id}/participants`, {
+            method: 'POST',
+            body: JSON.stringify({login: username}),
+            headers: {'content-type': 'application/json'}
+        });
+        if (response.ok) {
+            const newParticipants = await response.json();
+            const nextMeetings = meetings.map(m => {
+                if (m === meeting) {
+                    m.participants = newParticipants;
+                }
+                return m;
+            });
+            setMeetings(nextMeetings);
+        }
+    }
+
+    async function handleSignOut(meeting) {
+        const response = await fetch(`/api/meetings/${meeting.id}/participants/${username}`, {
+            method: 'DELETE',
+        });
+        if (response.ok) {
+            const newParticipants = await response.json();
+            const nextMeetings = meetings.map(m => {
+                if (m === meeting) {
+                    m.participants = m.participants = newParticipants;
+                }
+                return m;
+            });
+            setMeetings(nextMeetings);
+        }
+    }
+
+    useEffect(() => {
+        const fetchMeetings = async () => {
+            const response = await fetch(`/api/meetings`);
+            if (response.ok) {
+                const meetings = await response.json();
+                setMeetings(meetings);
+            }
+        };
+        fetchMeetings();
+    }, []);
+
+    return (
+        <div>
+            <h2>Zajęcia ({meetings.length})</h2>
+            {
+                addingNewMeeting
+                    ? <NewMeetingForm onSubmit={(meeting) => handleNewMeeting(meeting)}/>
+                    : <button onClick={() => setAddingNewMeeting(true)}>Dodaj nowe spotkanie</button>
+            }
+            {meetings.length > 0 &&
+                <MeetingsList meetings={meetings} username={username}
+                              onDelete={handleDeleteMeeting}
+                              onSignIn={handleSignIn}
+                              onSignOut={handleSignOut}/>}
+        </div>
+    )
+}
+*/
